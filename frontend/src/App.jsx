259626b71598d5985d221ca1676ab2ae7de672b9
tanmaisai1208 <tanmaisaich@gmail.com>
2026-02-10 22:03:50 +0530

    Confirm Weightage page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Dashboard from './pages/dashboard';
 import UploadMarks from "./pages/uploadMarks";
 import PreviousCourses from "./pages/previousCourses";
+import ConfirmWeightages from "./pages/ConfirmWeightages";
 import ScrollToTop from "./components/ScrollToTop";
 import './App.css';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/confirm-weightages" element={<ConfirmWeightages />} />
           <Route path="/previous-courses" element={<PreviousCourses />} />
           <Route path="/upload-marks" element={<UploadMarks />} />
         </Routes>
